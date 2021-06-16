7c3e279f495631bb0eeda76f37b77c3ab574d246
youngjedivuja <jvujovic1998@gmail.com>
2021-06-16 18:04:00 +0200

    Security annotations

---
+++ src/main/java/rs/istv/security/annotation/IsAuthenticated.java
@@ -0,0 +1,15 @@
+package rs.istv.security.annotation;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("isAuthenticated()")
+public @interface IsAuthenticated {
+}
---
+++ src/main/java/rs/istv/security/annotation/role/RequireBuyer.java
@@ -0,0 +1,13 @@
+package rs.istv.security.annotation.role;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole('ROLE_BUYER')")
+public @interface RequireBuyer {
+}
